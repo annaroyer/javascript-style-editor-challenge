7f8693cde032d42f4ac7709240550e187eec2d47
Anna Royer <anna.royer@colorado.edu>
2018-04-27 13:47:51 -0600

    add jquery so form will update element style on page on submit

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   // your code goes here.
+  // 1. **retrieving** the values from the form,
+  const selector = $("input[name='selector']")
+  const property = $("input[name='property']")
+  const value = $("input[name='value']")
+  // 2. **passing** those values into the correct jQuery function for manipulating CSS, and
+  // 3. **binding** the previous two parts to a particular event using an event handler.
+  $('form').submit(function(){
+    $(selector.val()).css(property.val(), value.val())
+    event.preventDefault()
+  })
 });
